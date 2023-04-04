514495a98a9ab853c5c790010c63a0ee0aeb05c6
ZhouGree <zzhougelbebe@outlook.com>
2023-04-05 00:49:32 +0800

    Initial commit

---
+++ src/main/java/com/zhou/pojo/Users.java
@@ -0,0 +1,30 @@
+package com.zhou.pojo;
+
+public class Users {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>TITLE</title>
+</head>
+<body>
+<h2>
+    <form action="/WebDemo/demo3" method ="get">
+    <h1>Hello! </h1>
+</form>
+</h2>
+</body>
+</html>
